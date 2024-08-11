62e87d53f9f0a571c5307632c39bfd4dbfeef93f
Grant Mukai <hello@grantmukai.com>
2024-08-11 11:09:54 -0400

    cv en ca ajouté

--- generer_cv.js
+++ generer_cv.js
@@ -2,15 +2,40 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 // Lire le fichier JSON
-const cvDonnees = JSON.parse(fs.readFileSync('cv_cafr.json', 'utf8'));
+//const cvDonnees = JSON.parse(fs.readFileSync('cv_cafr.json', 'utf8'));
 
 // Lire le gabarit EJS
-const gabarit_cv = fs.readFileSync('gabarit_cv.ejs', 'utf8');
+//const gabarit_cv = fs.readFileSync('gabarit_cv.ejs', 'utf8');
 
 // Générer le HTML
-const contenuHtml = ejs.render(gabarit_cv, { cv_cafr: cvDonnees });
+//const contenuHtml = ejs.render(gabarit_cv, { cv_cafr: cvDonnees });
 
 // Écrire le fichier HTML
-fs.writeFileSync('src/vues/composants/cv_cafr.html', contenuHtml, 'utf8');
+//fs.writeFileSync('src/vues/composants/cv_cafr.html', contenuHtml, 'utf8');
 
-console.log('Le fichier HTML a été généré avec succès !');
+//console.log('Le fichier HTML a été généré avec succès !');
+
+// ***
+
+// Fonction pour générer un CV
+function genererCV(entreeJson, fichierGabarit, sortieHtml) {
+    // Lire le fichier JSON
+    const donnees = JSON.parse(fs.readFileSync(entreeJson, 'utf8'));
+
+    // Lire le gabarit EJS
+    const gabarit = fs.readFileSync(fichierGabarit, 'utf8');
+
+    // Générer le HTML
+    const html = ejs.render(gabarit, donnees);
+
+    // Écrire le fichier HTML
+    fs.writeFileSync(sortieHtml, html, 'utf8');
+    console.log('Généré ${sortieHtml}');
+
+}
+
+// Générer le CV en français
+genererCV('cv_cafr.json', 'gabarit_cv.ejs', 'src/vues/composants/cv_cafr.html');
+
+// Générer le CV en anglais
+genererCV('cv_caen.json', 'gabarit_cv.ejs', 'src/vues/composants/cv_caen.html');
