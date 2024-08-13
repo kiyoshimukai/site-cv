070827f408ae42db77e58d6df9c4814e744fe9c5
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2024-08-13 19:51:17 -0400

    CV gt ajouté

--- src/js/changerLangue.js
+++ src/js/changerLangue.js
@@ -47,3 +47,13 @@ document.getElementById('drapeau_mx').addEventListener('click', function() {
         })
         .catch(error => console.error('Erreur lors du chargement du CV en espagnol méxicain', error));
 });
+
+document.getElementById('drapeau_gt').addEventListener('click', function() {
+    // Changer le contenu du CV en espagnol guatémaltèque
+    fetch('cv_gtes.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('cv').innerHTML = data;
+        })
+        .catch(error => console.error('Erreur lors du chargement du CV en espagnol guatémaltèque'));
+});
